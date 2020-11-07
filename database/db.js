89369fc9764ef881b2320faa1426211ceba3a455
GustaGuima <guga_cordeiro180@hotmail.com>
2020-11-07 09:42:29 -0300

    Banco

--- database/db.js
+++ database/db.js
@@ -1,16 +1,7 @@
 const Sequelize = require('sequelize')
 const db = {}
-const sequelize = new Sequelize('logicafacil', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    operatorAliases: false,
-
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres'
 })
 
 db.sequelize = sequelize
